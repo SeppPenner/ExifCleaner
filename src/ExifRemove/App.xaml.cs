0be5279d2cd62509f49f9291bbc79dc5f30b634b
SeppPenner <HansM2013@gmx.de>
2021-01-07 15:09:08 +0100

    Added missing code comments.

--- src/ExifRemove/App.xaml.cs
+++ src/ExifRemove/App.xaml.cs
@@ -1,9 +1,18 @@
-﻿using System.Windows;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Hämmer Electronics">
+//   Copyright (c) All rights reserved.
+// </copyright>
+// <summary>
+//   The application class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Windows;
 
 namespace ExifRemove
 {
     /// <summary>
-    ///     Interaktionslogik für "App.xaml"
+    ///  The application class.
     /// </summary>
     public partial class App : Application
     {
